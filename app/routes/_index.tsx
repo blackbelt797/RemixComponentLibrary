0398f32a2d44d99cb9de1171868aa4664c9470cf
Gregg Cline <ggcline82@gmail.com>
2023-10-15 20:24:25 -0700

    Building out different compnents. Also playing with over all layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,75 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+ return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+ return (
+  <div className="flex flex-col items-center justify-center ">
+   <nav className="flex flex-row justify-around my-6 w-full">
+    <a href="/">Buttons</a>
+    <a href="/">Navigation</a>
+    <a href="/">Forms</a>
+    <a href="/">Cards</a>
+   </nav>
+
+   {/* Buttons */}
+   <div className="flex flex-row justify-center gap-8">
+    <button
+     type="button"
+     className="text-white bg-blue-500 hover:bg-blue-700 w-28 px-5 py-2.5 mr-2 mb-2 rounded-lg cursor-pointer">
+     Button
+    </button>
+    <button
+     type="button"
+     className="text-white bg-green-500 hover:bg-green-700 w-28 px-5 py-2.5 mr-2 mb-2 rounded-lg cursor-pointer">
+     Button
+    </button>
+    <button
+     type="button"
+     className="text-white bg-red-500 hover:bg-red-700 w-28 px-5 py-2.5 mr-2 mb-2 rounded-lg  cursor-pointer">
+     Button
+    </button>
+    <button
+     type="button"
+     className="text-white bg-indigo-500 hover:bg-indigo-700 w-28 px-5 py-2.5 mr-2 mb-2  rounded-lg cursor-pointer">
+     Button
+    </button>
+    <button
+     type="button"
+     className="text-white bg-purple-500 hover:bg-purple-700 w-28 px-5 py-2.5 mr-2 mb-2  rounded-lg cursor-pointer">
+     Button
+    </button>
+   </div>
+   {/* Spacer */}
+   <div className="my-8"></div>
+
+   {/* Navigation */}
+   <div className="flex bg-blue-900 w-full py-8 justify-end pr-6 ">
+    <ul className="flex gap-4">
+     <li>
+      <a href="#" className="text-green-300">
+       About
+      </a>
+     </li>
+     <li>
+      <a href="#" className="text-green-300">
+       Info
+      </a>
+     </li>
+     <li>
+      <a href="#" className="text-green-300">
+       Services
+      </a>
+     </li>
+     <li>
+      <a href="#" className="text-green-300">
+       Contact
+      </a>
+     </li>
+    </ul>
+   </div>
+  </div>
+ );
 }
